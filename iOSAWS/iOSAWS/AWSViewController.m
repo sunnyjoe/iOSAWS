93a6d7ef348c21c4dbc0b6bfcbad2594683326b7
sunnyjoe <jiaoqingsunny@gmail.com>
2016-08-11 18:24:32 +0800

    tokyobucketstart upload success

--- iOSAWS/iOSAWS/AWSViewController.m
+++ iOSAWS/iOSAWS/AWSViewController.m
@@ -8,6 +8,7 @@
 
 #import "AWSViewController.h"
 #import <AWSCore/AWSCore.h>
+#import <AWSS3/AWSS3.h>
 
 @interface AWSViewController ()
 
@@ -19,12 +20,42 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    AWSCognitoCredentialsProvider *credentialsProvider = [[AWSCognitoCredentialsProvider alloc] initWithRegionType:AWSRegionAPNortheast1 identityPoolId:@"ap-northeast-1_scINcU7Cw"];
+    AWSCognitoCredentialsProvider *credentialsProvider = [[AWSCognitoCredentialsProvider alloc]
+                                                          initWithRegionType:AWSRegionAPNortheast1
+                                                          identityPoolId:@"ap-northeast-1:41fbe60a-1270-4a84-b630-696f5af28430"];
     
-    AWSServiceConfiguration *configuration = [[AWSServiceConfiguration alloc] initWithRegion:AWSRegionAPNortheast1
-                                                                         credentialsProvider:credentialsProvider];
+    AWSServiceConfiguration *configuration = [[AWSServiceConfiguration alloc] initWithRegion:AWSRegionAPNortheast1 credentialsProvider:credentialsProvider];
     
-    AWSServiceManager.defaultServiceManager.defaultServiceConfiguration = configuration;
+    [AWSServiceManager defaultServiceManager].defaultServiceConfiguration = configuration;
+    
+    
+  
+    // create a local image that we can use to upload to s3
+    NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"flower.jpg"];
+    NSData *imageData = UIImagePNGRepresentation([UIImage imageNamed:@"flower.jpg"]);
+    [imageData writeToFile:path atomically:YES];
+    NSURL *url = [[NSURL alloc] initFileURLWithPath:path];
+    
+    
+    AWSS3TransferManagerUploadRequest *uploadRequest = [AWSS3TransferManagerUploadRequest new];
+    uploadRequest.bucket = @"tokyobucketstart";
+    uploadRequest.key = @"test.txt";
+    uploadRequest.contentType = @"image/jpg";
+    uploadRequest.body = url;
+ //   uploadRequest.contentLength = [NSNumber numberWithInteger:[imgData length]];
+    
+
+    AWSS3TransferManager *transferManager = [AWSS3TransferManager defaultS3TransferManager];
+    
+    [[transferManager upload:uploadRequest] continueWithExecutor:[AWSExecutor mainThreadExecutor]
+                                                       withBlock:^id(AWSTask *task) {
+                                                           if (task.error != nil) {
+                                                               NSLog(@"Error %s %@", uploadRequest.key, task.error);
+                                                           }
+                                                           else { NSLog(@"Upload completed"); }
+                                                           return nil;
+                                                       }];
+    
 }
 
 
